b5cbb25100448092cadb78b9fe77c9d9643034c9
Théo Gicquel <theo.gicquel.work@gmail.com>
2023-04-22 18:30:54 +0200

    test file routing

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <Navbar />
+    <div class="flex h-auto">
+        <div class="m-auto">
+<h1 class="text-3xl font-bold underline">
+      Hi i'm Theo
+    </h1>
+    <button >
+      <a  class="btn btn-primary" href="/static/cv.pdf">CV</a>
+    </button>
+        </div>
+    
+    </div>
+
+  </template>   
