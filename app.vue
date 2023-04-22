385d3a791e6e4ccae7ae7e756344fe235da093ad
Théo Gicquel <theo.gicquel.work@gmail.com>
2023-04-22 18:01:04 +0200

    setup daisyui

--- app.vue
+++ app.vue
@@ -4,5 +4,8 @@
   </div>
   <h1 class="text-3xl font-bold underline">
     Hello world!
+    <a class="btn btn-disabled" href="https://nuxtjs.org" target="_blank">
+      Documentation
+      </a>
   </h1>
 </template>
